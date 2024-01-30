13b4e4d7c26873014272380233707e33c6a86b62
vitaly-goryaynov <diagen3688@mail.ru>
2024-01-30 16:03:45 +0300

    6 hw

--- hw_6/hw_6.py
+++ hw_6/hw_6.py
@@ -1,4 +1,4 @@
-# Задание № 6.1
+# # Задание № 6.1
 # class Animal:
 #     def __init__(self, name, species, age=0, weight=0, is_domestic=True):
 #         self.__name = name
@@ -13,38 +13,38 @@
 #     def get_name(self) -> str:
 #         return self.__name
 #
-#     def set_name(self, name:str) -> str:
+#     def set_name(self, name:str):
 #         self.__name = name
 #
 #     def get_species(self) -> str:
 #         return self.__species
 #
-#     def set_species(self, species:str) -> str:
+#     def set_species(self, species:str):
 #         self.__species = species
 #
 #     def get_age(self) -> int:
 #         return self.__age
 #
-#     def set_age(self, age:int) -> int:
+#     def set_age(self, age:int):
 #         self.__age = age
 #
 #     def get_weight(self) -> float:
 #         return self.__weight
 #
-#     def set_weight(self, weight:float) -> float:
+#     def set_weight(self, weight:float):
 #         self.__weight = weight
 #
 #     def get_is_domestic(self) -> bool:
 #         return self.__is_domestic
 #
-#     def set_is_domestic(self, is_domestic: bool) -> bool:
+#     def set_is_domestic(self, is_domestic: bool):
 #         self.__is_domestic = is_domestic
 #
 #     def make_sound(self, sound:str) -> str:
 #         return f'Животное издало звук {sound}'
 #
 #
-#     def feed(self, food:float) -> float or str:
+#     def feed(self, food:float) -> str:
 #         if food > 0:
 #             self.__weight = self.__weight + food
 #         else:
@@ -60,18 +60,17 @@
 # 6.2
 
 class Pizza:
-    def __init__(self, name, ingredients, size, price=None):
+    def __init__(self, name, size, price=None):
         self.__name = name
-        self.__ingredients = ingredients
+        self.__ingredients = []
         self.__size = size
         self.__price = price
 
     def __str__(self):
-        pass
+        return f'Название пиццы {self.__name}, список ингридиентов {self.__ingredients}, \nразмер пиццы {self.__size}, цена пиццы {self.__price}'
 
-    def add_ingredients(self, ingredients):
-        self.__count_ingredients = []
-        self.__count_ingredients.append(ingredients)
+    def add_ingredients(self, ingredient):
+        self.__ingredients.append(ingredient)
     def calculate_price(self):
         pass
 

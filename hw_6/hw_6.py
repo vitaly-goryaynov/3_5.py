69f59a3837361640042d7ff47819f6591cd9e1d3
vitaly-goryaynov <diagen3688@mail.ru>
2024-01-30 23:56:04 +0300

    6 hw 3

--- hw_6/hw_6.py
+++ hw_6/hw_6.py
@@ -59,21 +59,109 @@
 
 # 6.2
 
-class Pizza:
-    def __init__(self, name, size, price=None):
+# class Pizza:
+#     def __init__(self, name, size=18, price=50):
+#         self.__name = name
+#         self.__ingredients = []
+#         self.__size = size
+#         self.__price = price
+#
+#
+#     def __str__(self):
+#         return f'Название пиццы {self.__name}, список ингридиентов {self.__ingredients}, \nразмер пиццы {self.__size}, цена пиццы {self.__price}'
+#
+#     def get_name(self):
+#         return self.__name
+#
+#     def set_name(self, name):
+#         self.__name = name
+#
+#     def get_size(self):
+#         return self.__size
+#
+#     def get_price(self):
+#         return self.__price
+#
+#     def add_ingredients(self, ingredient):
+#         self.__ingredients.append(ingredient)
+#
+#     def get_ingredients(self):
+#         return self.__ingredients
+#
+#     def calculate_price(self):
+#         count = len(self.__ingredients)
+#         if count > 0:
+#             self.__price = self.__price * count
+#
+#     def order_size(self, size):
+#         if size == 18:
+#             self.__size = 18
+#             self.__price = self.__price + 200
+#         elif size == 25:
+#             self.__size = 25
+#             self.__price= self.__price + 300
+#         elif size == 30:
+#             self.__size = 30
+#             self.__price = self.__price + 400
+#         elif size == 35:
+#             self.__size = 35
+#             self.__price = self.__price + 500
+#         else:
+#             print('Недопустимый размер пиццы')
+#
+#
+# p1 = Pizza('peperony', )
+# p1.order_size(25)
+# p1.add_ingredients('salyami')
+# p1.add_ingredients('ogurci')
+# p1.calculate_price()
+# print(p1)
+
+# 6.3
+
+class Cafe:
+    def __init__(self, name) -> None:
         self.__name = name
-        self.__ingredients = []
-        self.__size = size
-        self.__price = price
+        self.__menu = []
 
-    def __str__(self):
-        return f'Название пиццы {self.__name}, список ингридиентов {self.__ingredients}, \nразмер пиццы {self.__size}, цена пиццы {self.__price}'
+    def __str__(self) -> str:
+        return f'Кафе {self.__name}. Список блюд: {self.__menu}'
 
-    def add_ingredients(self, ingredient):
-        self.__ingredients.append(ingredient)
-    def calculate_price(self):
+    def get_name(self) -> str:
+        return self.__name
+
+    def set_name(self, name: str) -> None:
+        self.__name = name
+
+    def order_products(self):
         pass
 
+    def display_menu(self) -> list:
+        return self.__menu
+
+    def add_dish(self, dish: str) -> None:
+        self.__menu.append(dish)
 
 
+class Suppler:
+    def __init__(self, name) -> None:
+        self.__name = name
+        self.__product_list = []
 
+    def __str__(self) -> str:
+        return f'Поставщик {self.__name}, \nсписок поставляемых продуктов {self.__product_list}'
+
+    def get_name(self) -> str:
+        return self.__name
+
+    def set_name(self, name: str):
+        self.__name = name
+
+    def display_products(self) -> list:
+        return self.__product_list
+
+    def add_product(self, product: str):
+        self.__product_list.append(product)
+
+    def dell_product(self, product: str):
+        self.__product_list.remove(product)
